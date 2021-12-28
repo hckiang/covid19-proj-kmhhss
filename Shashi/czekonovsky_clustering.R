592532239e6d8b3c156ae4922ea86d0e8ff3bbe4
Shashi Nagarajan Iyer <shashiniyer@gmail.com>
2021-12-29 00:48:57 +0100

    backup_29_12_2021
    backup

--- Shashi/czekonovsky_clustering.R
+++ Shashi/czekonovsky_clustering.R
@@ -1,89 +1,66 @@
 # set up data
-
-all_four_distances <- read.csv(
-  "./data/clustering_distance_datasets/all_four_distances.csv",
-  row.names = 1)
-
-all_except_geog <- read.csv(
-  "./data/clustering_distance_datasets/all_except_geog.csv",
-  row.names = 1)
-
-all_except_adj <- read.csv(
-  "./data/clustering_distance_datasets/all_except_adj.csv",
-  row.names = 1)
-
-all_except_pop <- read.csv(
-  "./data/clustering_distance_datasets/all_except_pop.csv",
-  row.names = 1)
-
-pop_pop_dens <- read.csv(
-  "./data/clustering_distance_datasets/pop_pop_dens.csv",
-  row.names = 1)
-
-pop_dens <- read.csv(
-  "./data/clustering_distance_datasets/pop_dens.csv",
-  row.names = 1)
-
-pop_dens_adj <- read.csv(
-  "./data/clustering_distance_datasets/pop_dens_adj.csv",
-  row.names = 1)
-
-pop_dens_geog <- read.csv(
-  "./data/clustering_distance_datasets/pop_dens_geog.csv",
-  row.names = 1)
-
-ts_cases_100k_wo_dtw <-read.csv(
-  "./data/clustering_distance_datasets/ts_cases_100k_wo_dtw.csv",
-  row.names = 1)
-
-ts_deaths_100k_wo_dtw <-read.csv(
-  "./data/clustering_distance_datasets/ts_deaths_100k_wo_dtw.csv",
-  row.names = 1)
-
-ts_cases_100k_w_dtw <-read.csv(
-  "./data/clustering_distance_datasets/ts_cases_100k_w_dtw.csv",
-  row.names = 1)
-
-ts_deaths_100k_w_dtw <-read.csv(
-  "./data/clustering_distance_datasets/ts_deaths_100k_w_dtw.csv",
-  row.names = 1)
-
-total_mortality_100K_wo_dtw <-read.csv(
-  "./data/clustering_distance_datasets/total_mortality_100K_wo_dtw.csv",
-  row.names = 1)
-
-excess_mortality_100K_wo_dtw <-read.csv(
-  "./data/clustering_distance_datasets/excess_mortality_100K_wo_dtw.csv",
-  row.names = 1)
-
-total_mortality_100K_w_dtw <-read.csv(
-  "./data/clustering_distance_datasets/total_mortality_100K_w_dtw.csv",
-  row.names = 1)
-
-excess_mortality_100K_w_dtw <-read.csv(
-  "./data/clustering_distance_datasets/excess_mortality_100K_w_dtw.csv",
+cluster_data <- read.csv("./data/clustering_distance_datasets/cluster_data.csv",
   row.names = 1)
 
 # import library
 library(RMaCzek)
 
+# compute Czek matrix
+czek_matrix_out <- czek_matrix(cluster_data, scale_data = FALSE, n_classes = 9)
+ordering <- attr(czek_matrix_out, "order")
+
 # visualise
-plot(czek_matrix(all_four_distances, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(all_except_geog, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(all_except_adj, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(all_except_pop, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(pop_pop_dens, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(pop_dens, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(pop_dens_adj, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(pop_dens_geog, scale_data = FALSE, n_classes = 6))
-
 par(mar=c(8,8,4,1))
-plot(czek_matrix(ts_cases_100k_wo_dtw, scale_data = FALSE, n_classes = 5))
-plot(czek_matrix(ts_deaths_100k_wo_dtw, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(ts_cases_100k_w_dtw, scale_data = FALSE, n_classes = 6))
-plot(czek_matrix(ts_deaths_100k_w_dtw, scale_data = FALSE, n_classes = 6))
+plot(czek_matrix_out)
 
-plot(czek_matrix(total_mortality_100K_wo_dtw, scale_data = FALSE, n_classes = 5))
-plot(czek_matrix(excess_mortality_100K_wo_dtw, scale_data = FALSE, n_classes = 4))
-plot(czek_matrix(total_mortality_100K_w_dtw, scale_data = FALSE, n_classes = 3))
-plot(czek_matrix(excess_mortality_100K_w_dtw, scale_data = FALSE, n_classes = 8))
+## stepwise dendrogram - reference: Müllner, D., 2011. Modern hierarchical, agglomerative clustering algorithms. arXiv preprint arXiv:1109.2378
+
+# initialise
+S <- ordering
+N <- length(S) # number of nodes
+size <- rep(1, N)
+dendrogram <- matrix(nrow = nrow(cluster_data) - 1, ncol = 4)
+n <- N
+
+# compute distance matrix (vector); distance between each (node, next node) pair; "next" defined as per ordering
+d <- sapply(1:(N-1), function(x)
+ sqrt(sum((cluster_data[S[x], ]-cluster_data[S[x + 1], ])^2)))
+
+# update dendrogram matrix
+for (i in 1:nrow(dendrogram)) {
+  
+  # compute one pair of nodes with minimal distance
+  min_dist_ind = which(d == min(d))
+  
+  if (length(min_dist_ind) > 1) {
+    
+    # randomly break ties
+    min_dist_ind = sample(min_dist_ind, 1)
+    
+  }
+  
+  # create new node label
+  n = n + 1
+  
+  # update size vector
+  size[n] <- size[S[min_dist_ind]] + size[S[min_dist_ind + 1]]
+  
+  # update dendrogram matrix
+  dendrogram[i, ] = 
+    c(S[min_dist_ind], S[min_dist_ind + 1], d[min_dist_ind], size[n])
+  
+  # update ordering
+  S[min_dist_ind] = n
+  S = S[-(min_dist_ind + 1)]
+  
+  # update distance matrix (vector)
+  d = d[-min_dist_ind]
+
+  # update number of nodes
+  N <- length(S)
+  
+}
+
+# write dendrogram matrix to file
+write.csv(dendrogram, row.names = F,
+          file = "./data/clustering_distance_datasets/czekanowski_dendrogram.csv")
